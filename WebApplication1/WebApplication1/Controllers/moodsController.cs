56e251d532ddc5002d62ab9d15c71c8e80828d5c
Oscar Nicolas Landivar Aguilera <a2019114151@estudiantes.upsa.edu.bo>
2020-12-12 09:32:45 -0400

    finalizado

---
+++ WebApplication1/WebApplication1/Controllers/moodsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/operaciones")]
+    public class moodsController : ApiController
+    {
+        [HttpGet]
+        [Route("{number:int}")]
+        public mood Operations(int number)
+        {
+            mood model = new mood();
+            model.numero = number;
+
+            if (number < 0)
+                model.mensaje = "ERROR";
+
+            if (number == 0)
+                model.mensaje = "Realizado por okar";
+
+            if (number > 0)
+                model.mensaje = "Usted ingreso el numero " + number.ToString();
+
+            return model;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/mood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class mood
+    {
+        [Key]
+        public int numero { get; set; }
+        public string mensaje { get; set; }
+    }
+}
